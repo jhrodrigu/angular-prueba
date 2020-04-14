70267283dd3bc85cbfc5cc92a78cbc6c4a2846c8
jhrodrigu <jhrodrigu@hotmail.com>
2020-04-14 03:01:17 -0500

    2 files modified, 2 added

---
+++ src/app/zapatillas/zapatilla.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+
+@Component({
+    selector: 'marcazapatillas',
+    templateUrl: './zapatillas.component.html'
+})
+    export class ZapatillaComponent{
+      nombre: 'Yonny';
+    };
